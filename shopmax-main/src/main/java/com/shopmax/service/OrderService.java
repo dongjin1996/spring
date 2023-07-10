c37d741c5a2b88ec98a9db0288bf115a1b2aaa81
github dongjin1996 <github ehwl01@naver.com>
2023-07-10 18:18:44 +0900

    07/10

--- shopmax-main/src/main/java/com/shopmax/service/OrderService.java
+++ shopmax-main/src/main/java/com/shopmax/service/OrderService.java
@@ -3,14 +3,21 @@ package com.shopmax.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.shopmax.dto.OrderDto;
+import com.shopmax.dto.OrderHistDto;
+import com.shopmax.dto.OrderItemDto;
 import com.shopmax.entity.Item;
+import com.shopmax.entity.ItemImg;
 import com.shopmax.entity.Member;
 import com.shopmax.entity.Order;
 import com.shopmax.entity.OrderItem;
+import com.shopmax.repository.ItemImgRepository;
 import com.shopmax.repository.ItemRepository;
 import com.shopmax.repository.MemberRepository;
 import com.shopmax.repository.OrderRepository;
@@ -27,6 +34,7 @@ public class OrderService {
 		private final ItemRepository itemRepository;
 		private final MemberRepository memberRepository;
 		private final OrderRepository orderRepository;
+		private final ItemImgRepository itemImgRepository;
 		
 		// 주문
 		public Long order(OrderDto orderDto, String email) {
@@ -48,5 +56,41 @@ public class OrderService {
 			
 			return order.getId();
 		}
+		
+		
+		//주문목록을 가져오는 서비스
+		@Transactional(readOnly = true)
+		public Page<OrderHistDto> getOrderList(String email, Pageable pageable) {
+			//1. 유저 아이디와 페이징 조건을 이용하여 주문 목록을 조회
+			List<Order> orders = orderRepository.findOrders(email, pageable);
+			
+			//2. 유저의 주문 총 개수를 구한다.
+			Long totalCount = orderRepository.countOrder(email);
+			
+			//3. 주문 리스트를 순회하면서 구매 이력 페이지에 전달할 DTO(OrderHistDto)를 생성
+			
+			List<OrderHistDto> orderHistDtos = new ArrayList<>();
+			
+			for(Order order : orders) {
+				OrderHistDto orderHistDto = new OrderHistDto(order);
+				List<OrderItem> orderItems = order.getOrderItems();
+				
+				for(OrderItem orderItem : orderItems) {
+					//상품의 대표 이미지 가져오기
+					ItemImg itemImg = itemImgRepository
+									.findByItemIdAndRepimgYn(orderItem.getItem().getId(), "Y");
+					OrderItemDto orderItemDto = new OrderItemDto(orderItem, itemImg.getImgUrl());
+					orderHistDto.addOrderItemDto(orderItemDto);
+				}
+				
+				orderHistDtos.add(orderHistDto);
+			}
+			
+			
+			return new PageImpl<>(orderHistDtos, pageable, totalCount); //4.페이지 구현 객체를 생성하여 return
+		}
+		
+		
+		
 	}
 
