0c9cd3a09d67e6a3baaf5f16be925e1d74d1fd75
dongjin1996 <ehwl01@naver.com>
2023-07-31 17:17:58 +0900

    07/31

--- fastfood/src/main/java/com/fastfood/config/MailConfig.java
+++ fastfood/src/main/java/com/fastfood/config/MailConfig.java
@@ -1,5 +1,7 @@
 package com.fastfood.config;
 
+import java.util.Properties;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -7,10 +9,33 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class MailConfig {
+	
 	@Bean
 	public JavaMailSender javaMailService() {
 		JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
 		
+		javaMailSender.setHost("smpt.google.com");
+		javaMailSender.setUsername("doji1996@gmail.com");
+		javaMailSender.setPassword("ttdkwyymjaqcqvhg");
 		
+		javaMailSender.setPort(587);
+		
+		javaMailSender.setJavaMailProperties(getMailProperties());
+		
+		return javaMailSender;
+		
+	}
+	
+	private Properties getMailProperties() {
+		Properties properties = new Properties();
+		
+		properties.setProperty("mail.transport.protocol", "smtp");
+		properties.setProperty("mail.smtp.auth", "true");
+		properties.setProperty("mail.smtp.starttls.enable", "true");
+		properties.setProperty("mail.debug", "true");
+		properties.setProperty("mail.smtp.ssl.trust", "smtp.google.com");
+		properties.setProperty("mail.smtp.ssl.enable", "true"); 
+		
+		return properties;
 	}
 }
